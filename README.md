fafe52816517ff2c44635328b88c9cbd69190e41
msg <gultekinmsg@gmail.com>
2019-06-23 23:34:25 +0300

    added README created hello world project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Java Samples
+Multimodule Maven project, contains basic java exercises.
+
+To build project; `mvn clean install`
+
+# SAMPLES LIST
+
+**1- Hello World**
+
+Path: `/helloworld`
---
+++ helloworld/src/main/java/com/gultekinmsg/javasamples/helloworld/Main.java
@@ -0,0 +1,26 @@
+package com.gultekinmsg.javasamples.helloworld;
+
+public class Main {
+
+    public static void main(String [] args){
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Mami!");
+
+        int myFirtsNumber = (10 + 5) + (2 * 10);
+        int mySecodNumber = 12;
+        int myThirdNumber = myFirtsNumber * 2;
+
+        int myTotal = myFirtsNumber + mySecodNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myFirtsNumber);
+        System.out.println("myFirstNumber");
+        System.out.println(myThirdNumber);
+
+        System.out.println(myTotal);
+
+        System.out.println(myLastOne);
+
+    }
+}
